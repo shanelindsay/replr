551b08e241b9fc3e4b8b17f02ec6784240ebe814
Shane Lindsay <shane.lindsay@gmail.com>
2025-06-19 16:03:54 +0100

    Set summary flag for preview tests

--- tests/testthat/test-preview-option.R
+++ tests/testthat/test-preview-option.R
@@ -4,7 +4,7 @@ test_that("default preview_rows is 5", {
   on.exit(ps$kill())
   Sys.sleep(1)
 
-  res <- replr::exec_code("data.frame(a=1:10)", port=8130, plain = FALSE)
+  res <- replr::exec_code("data.frame(a=1:10)", port=8130, plain = FALSE, summary = TRUE)
   expect_equal(length(res$result_summary$preview), 5)
 })
 
@@ -14,7 +14,7 @@ test_that("preview_rows option can be changed", {
   on.exit(ps$kill())
   Sys.sleep(1)
   replr::exec_code("options(replr.preview_rows=3)", port=8131, plain = FALSE)
-  res <- replr::exec_code("data.frame(a=1:10)", port=8131, plain = FALSE)
+  res <- replr::exec_code("data.frame(a=1:10)", port=8131, plain = FALSE, summary = TRUE)
 
   expect_equal(length(res$result_summary$preview), 3)
 })
