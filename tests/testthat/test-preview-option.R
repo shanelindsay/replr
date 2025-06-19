f3528d5843c119d2668bc9fb7e55b185477aa433
Shane Lindsay <shane.lindsay@gmail.com>
2025-06-19 15:15:31 +0100

    Expand CLI demo with text mode and error handling

--- tests/testthat/test-preview-option.R
+++ tests/testthat/test-preview-option.R
@@ -3,7 +3,7 @@ test_that("default preview_rows is 5", {
   ps <- processx::process$new("Rscript", c(system.file("scripts", "replr_server.R", package="replr"), "--port", 8130, "--background"))
   on.exit(ps$kill())
   Sys.sleep(1)
-  res <- replr::exec_code("data.frame(a=1:10)", port=8130)
+  res <- replr::exec_code("data.frame(a=1:10)", port=8130, plain = FALSE)
   expect_equal(length(res$result_summary$preview), 5)
 })
 
@@ -12,7 +12,7 @@ test_that("preview_rows option can be changed", {
   ps <- processx::process$new("Rscript", c(system.file("scripts", "replr_server.R", package="replr"), "--port", 8131, "--background"))
   on.exit(ps$kill())
   Sys.sleep(1)
-  replr::exec_code("options(replr.preview_rows=3)", port=8131)
-  res <- replr::exec_code("data.frame(a=1:10)", port=8131)
+  replr::exec_code("options(replr.preview_rows=3)", port=8131, plain = FALSE)
+  res <- replr::exec_code("data.frame(a=1:10)", port=8131, plain = FALSE)
   expect_equal(length(res$result_summary$preview), 3)
 })
